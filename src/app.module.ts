429d82c669115f247db7a4dc87d0bea33505a162
NaolDaba <nahafile@gmail.com>
2024-01-12 21:29:25 +0300

    added role.guards.ts file and updated the app.modules.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { RecipeModule } from './recipe/recipe.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
-import { Reflector } from '@nestjs/core';
+import { APP_GUARD, Reflector } from '@nestjs/core';
+import { RolesGuard } from './guards/roles.guard';
 import { JwtStrategy } from './auth/strategies/jwt.strategy';
+import { UserModule } from './user/user.module';
+import * as bodyParser from 'body-parser';
+// import { CorsModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
@@ -14,7 +19,14 @@ import { JwtStrategy } from './auth/strategies/jwt.strategy';
     }),
     MongooseModule.forRoot(process.env.DB_URI),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    AuthModule],
-  providers: [ Reflector, JwtStrategy]
+    AuthModule, RecipeModule, UserModule,
+    
+  ],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: RolesGuard,
+  }, Reflector, JwtStrategy]
 })
-export class AppModule { }
+export class AppModule { 
+ 
+}
